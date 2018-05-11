6e479553a8b5f5d3043f2d06cf97ac204d33cc43
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2018-05-10 23:15:53 -0700

    somthing

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+let dataSet1;
+let dataSet2;
+let dataSetFlag = 0;
+let dataSet;
+
+
 let width = 960,
     height = 500;
 
-// Define color scale
-const color = d3.scaleLinear()
-    .domain([25000, 85000])
-    .clamp(true)
-    .range(['#eef5f5', '#409A99']);
 
 const projection = d3.geoAlbers()
     .center([0.7, 52.958])
@@ -17,68 +18,180 @@ const projection = d3.geoAlbers()
 const path = d3.geoPath()
     .projection(projection);
 
-const svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height);
 
-// Legend
-const g = svg.append("g")
-    .attr("class", "legendThreshold")
-    .attr("transform", "translate(20,20)");
-
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", 0)
-    .attr("y", -6)
-    .text("Number of people who voted");
+const g = svg.append('g')
+    .attr('class', 'key')
+    .attr('transform', 'translate(0,40)');
 
 
-const labels = ['26,000 - 38,000', '38,000 - 51,000', '51,000 - 63,000', '63,000 - 75,000', '75,000 - 88,000', '>88,000'];
+const color1 = d3.scaleThreshold()
+    .domain([150, 200, 250, 300, 350, 400, 450, 500, 550])
+    .range(d3.schemeBlues[7]);
 
-const legend = d3.legendColor()
-    .labels(function (d) {
-        return labels[d.i];
-    })
-    .shapePadding(4)
-    .scale(color);
-
-svg.select(".legendThreshold")
-    .call(legend);
+const color2 = d3.scaleThreshold()
+    .domain([20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000])
+    .range(d3.schemeYlGn[7]);
 
 
-function parseData() {
-    d3.csv("data.csv", function (data) {
-        let regions = formatData(data);
-        d3.json('regions.geojson', function (error, mapData) {
-            const features = mapData.features;
-            svg.append("g")
-                .attr("class", "countries")
-                .selectAll("path")
-                .data(features)
-                .enter().append('path')
-                .attr('d', path)
-                .attr('vector-effect', 'non-scaling-stroke')
-                .style('fill', function (d) {
-                    return color(regions.get(d.properties.region_code).length)
-                });
+const x2 = d3.scaleSqrt()
+    .domain([20000, 100000])
+    .rangeRound([500, 1100]);
+
+const x1 = d3.scaleSqrt()
+    .domain([150, 550])
+    .rangeRound([500, 1100]);
+
+function createLegends() {
+    g.selectAll('rect')
+        .data(
+            color1.range().map(function (d) {
+                d = color1.invertExtent(d);
+                if (d[0] == null) d[0] = x1.domain()[0];
+                if (d[1] == null) d[1] = x1.domain()[1];
+                return d;
+            }))
+        .enter().append('rect')
+        .attr('height', 8)
+        .attr('x', function (d) {
+            return x1(d[0]);
+        })
+        .attr('width', function (d) {
+            return x1(d[1]) - x1(d[0]);
+        })
+        .attr('fill', function (d) {
+            return color1(d[0]);
+        });
+
+    g.append('text')
+        .attr('class', 'caption')
+        .attr('x', x1.range()[0])
+        .attr('y', -6)
+        .attr('fill', '#000')
+        .attr('text-anchor', 'start')
+        .attr('font-weight', 'bold')
+        .text('Population per square km');
+
+    g.call(d3.axisBottom(x1)
+        .tickSize(13)
+        .tickValues(color1.domain()))
+        .select(".domain")
+        .remove();
+
+    ///
+
+    g.selectAll('rect')
+        .data(
+            color2.range().map(function (d) {
+                d = color2.invertExtent(d);
+                if (d[0] == null) d[0] = x2.domain()[0];
+                if (d[1] == null) d[1] = x2.domain()[1];
+                return d;
+            }))
+        .enter().append('rect')
+        .attr('height', 8)
+        .attr('x', function (d) {
+            return x2(d[0]);
+        })
+        .attr('width', function (d) {
+            return x2(d[1]) - x2(d[0]);
+        })
+        .attr('fill', function (d) {
+            return color2(d[0]);
+        });
+
+    g.append('text')
+        .attr('class', 'caption')
+        .attr('x', x2.range()[0])
+        .attr('y', -6)
+        .attr('fill', '#000')
+        .attr('text-anchor', 'start')
+        .attr('font-weight', 'bold')
+        .text('Number of people who voted in 2011 consensus');
+
+    g.call(d3.axisBottom(x2)
+        .tickSize(13)
+        .tickValues(color2.domain()))
+        .select(".domain")
+        .remove();
+}
+
+
+$(function () {
+    $('.toggle').on('click', function (event) {
+        event.preventDefault();
+        // d3.select("rect").remove();
+        $(this).toggleClass('active');
+        if (dataSetFlag === 0) {
+            g.select('g').select('.countries1').style('opacity', 0);
+            g.select('g').select('.countries2').style('opacity', 1);
+        } else {
+            // g.select('.countries1').transition().duration(1000).style('opacity', 1);
+            g.select('g').select('.countries1').style('opacity', 1);
+            g.select('g').select('.countries2').style('opacity', 0);
+        }
+        dataSetFlag = dataSetFlag === 1 ? 0 : 1;
+    });
+});
+
+function loadDataSets() {
+    d3.csv('population_data.csv', function (data) {
+        dataSet1 = new Map();
+        data.forEach(function (d) {
+            dataSet1.set(d.Region, +d.People_per_Sq_km);
+        });
+        dataSet = dataSet1;
+    });
+    d3.csv('consensus_data.csv', function (data) {
+        dataSet2 = new Map();
+        data.forEach(function (d) {
+            if (dataSet2.has(d.Region)) {
+                let count = dataSet2.get(d.Region);
+                dataSet2.set(d.Region, ++count)
+            } else {
+                dataSet2.set(d.Region, 1);
+            }
         });
     });
+
 }
 
-
-function formatData(data) {
-    let regions = new Map();
-    data.forEach(function (d) {
-        if (regions.has(d.Region)) {
-            let arr = regions.get(d.Region);
-            arr.push(d.Person_ID);
-            regions.set(d.Region, arr)
-        } else {
-            regions.set(d.Region, [d.Person_ID]);
-        }
+function renderMap() {
+    d3.json('regions.geojson', function (error, mapData) {
+        const features = mapData.features;
+        // svg.append('g')
+        //     .attr('class', 'countries1')
+        //     .style('opacity', 0)
+        //     .selectAll('path')
+        //     .data(features)
+        //     .enter().append('path')
+        //     .attr('d', path)
+        //     .attr('vector-effect', 'non-scaling-stroke')
+        //     .style('fill', function (d) {
+        //         return color1(dataSet.get(d.properties.region_code))
+        //     });
+        svg.append('g')
+            .attr('class', 'countries2')
+            .style('opacity', 1)
+            .selectAll('path')
+            .data(features)
+            .enter().append('path')
+            .attr('d', path)
+            .attr('vector-effect', 'non-scaling-stroke')
+            .style('fill', function (d) {
+                return color2(dataSet.get(d.properties.region_code))
+            });
     });
-    return regions
 }
 
 
-parseData();
+$.when(
+    loadDataSets()
+).done(function (res1, res2) {
+    renderMap();
+    // createLegend(color1, x1)
+}).fail(function (err) {
+    console.log(err);
+});
